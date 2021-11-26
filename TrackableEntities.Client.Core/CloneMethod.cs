49302cea4481019e8a7e58b987650d80dc4e8a61
Rob Morriss <rmorriss@luminoruv.com>
2021-11-26 16:22:38 -0500

    Add warning about memberwise, currently broken

--- TrackableEntities.Client.Core/CloneMethod.cs
+++ TrackableEntities.Client.Core/CloneMethod.cs
@@ -3,7 +3,8 @@
     public enum CloneMethod
     {
         JsonSerialized,
-        Memberwise
+        Memberwise  //Has issue with multiple uses of an entity in a graph.
+                    //IE adding Customer with Customer->Address, Customer->Contact->Address where Address is the same.
     }
 
 
