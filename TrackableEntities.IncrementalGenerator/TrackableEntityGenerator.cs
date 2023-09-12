66df5cf2275e03323b5cf6b5294301c9298175c0
Rob Morriss <rmorriss@luminoruv.com>
2023-09-12 12:45:57 -0400

    Added ContractResolver to allow System.Text.Json to use JsonIgnore to trim returned data to tracked entities only.

--- TrackableEntities.IncrementalGenerator/TrackableEntityGenerator.cs
+++ TrackableEntities.IncrementalGenerator/TrackableEntityGenerator.cs
@@ -180,8 +180,7 @@ internal sealed class TrackableEntityCopyAttribute : Attribute
         var baseNameSpace = GetNamespace(distinctClasses.First());
         //var useShared = distinctClasses.Any(c => c.AttributeLists.Any(al => al.Attributes.Any(a => a.ArgumentList?.Arguments.Count > 0 && a.ArgumentList.Arguments[0].ToString() == "true")));
         var usings = GetUsingDirectives("TrackableEntity", distinctClasses);
-        //if the user is using Newtonsoft, it will deserialize all provided data. We can mark untracked properties with JsonIgnore. With System.Text.Json, JsonIgnore properties are not deserialized, they can't have the attribute.
-        List<ClientEntityToGenerate> entitiesToGenerate = GetTypesToGenerate(compilation, distinctClasses, usings.Contains("Newtonsoft.Json"), context.CancellationToken);
+        List<ClientEntityToGenerate> entitiesToGenerate = GetTypesToGenerate(compilation, distinctClasses, usings.Contains("Newtonsoft.Json") || usings.Contains("System.Text.Json.Serialization"), context.CancellationToken);
 
         // If there were errors in the ClassDeclarationSyntax, we won't create an
         // ClientEntityToGenerate for it, so make sure we have something to generate
